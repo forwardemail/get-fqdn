ebe84ddb0dcbc8b26e34609b1b3b485a676285ea
niftylettuce <niftylettuce@gmail.com>
2019-05-09 14:01:46 -0500

    fix: fixed ci issue with fqdn lookup

--- test/test.js
+++ test/test.js
@@ -1,4 +1,6 @@
 const test = require('ava');
+const isCI = require('is-ci');
+const { isFQDN } = require('validator');
 
 const getFQDN = require('..');
 
@@ -15,14 +17,29 @@ test.cb('callback returns FQDN', t => {
   });
 });
 
-test('throws error with local IP lookup', async t => {
-  const err = await t.throwsAsync(getFQDN);
-  t.regex(err.message, /getnameinfo ENOTFOUND/);
-});
-
-test.cb('callback throws error with local IP lookup', t => {
-  getFQDN(err => {
-    t.regex(err.message, /getnameinfo ENOTFOUND/);
-    t.end();
+if (isCI) {
+  test('does not throw  error with local IP lookup', async t => {
+    const fqdn = await getFQDN();
+    t.true(isFQDN(fqdn));
   });
-});
+
+  test.cb('callback does not throw error with local IP lookup', t => {
+    getFQDN((err, fqdn) => {
+      t.is(err, null);
+      t.true(isFQDN(fqdn));
+      t.end();
+    });
+  });
+} else {
+  test('throws error with local IP lookup', async t => {
+    const err = await t.throwsAsync(getFQDN);
+    t.regex(err.message, /getnameinfo ENOTFOUND/);
+  });
+
+  test.cb('callback throws error with local IP lookup', t => {
+    getFQDN(err => {
+      t.regex(err.message, /getnameinfo ENOTFOUND/);
+      t.end();
+    });
+  });
+}
